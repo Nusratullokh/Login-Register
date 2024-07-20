7be69f6863e85a6d26b8fcb8d0c5f63287a46e2d
Nusratullokh <nusratullokh7700@gmail.com>
2024-07-20 15:23:42 +0500

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,3 @@ function App() {
 }
 
 export default App
-// echo "# Login-Register" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/Nusratullokh/Login-Register.git
-// git push -u origin main
-
-// git remote add origin https://github.com/Nusratullokh/Login-Register.git
-// git branch -M main
-// git push -u origin main
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,39 @@
 import {useRoutes} from "react-router-dom"
-import Home from "./home/Home"
-import Auth from "./auth/Auth";
+import {  lazy } from "react";
+
+// import Suspense, { Loading } from "../utils";
+
+
+import  Suspense,  { Loading } from "../utils/index";
+
+
+const Home = lazy (() => import("./home/Home"))
+const Auth = lazy (() => import("./auth/Auth"))
+
+const Login = lazy(()=> import("./auth/login/Login"));
+const Register = lazy(()=> import("./auth/register/Register"));
+
+
 const RouteController = () => {
 
   return useRoutes ([
     {
-      path: "/",
-      element: <Home/>
+      path: "",
+      element: <Suspense><Home/></Suspense>
   },
   {
-      path: "/auth",
-      element: <Auth/>
+      path: "auth",
+      element: <Suspense><Auth/></Suspense>,
+      children: [
+        {
+          path: "",
+          element: <Suspense><Login/></Suspense>
+      },
+      {
+        path: "register",
+        element: <Suspense> <Register/></Suspense>
+    }
+      ]
   }
   ]  )
 }
